e003d9bc88090e07b3d9af359d9bf2f35ddc6f55
jei kwon <topjay52@gmail.com>
2025-11-21 00:19:25 +0900

    기본 스크린 ui 생성

---
+++ lib/features/auth/services/auth_service.dart
@@ -0,0 +1,17 @@
+//import 'package:firebase_auth/firebase_auth.dart';
+
+//이메일/비밀번호 회원가입 함수
+
+//이메일/비밀번호 로그인 함수
+
+//로그아웃 함수
+
+
+
+//Firebase Auth 연동 (firebase_auth) 및 초기 설정
+
+//Firestore에 users 컬렉션 설계
+
+//회원가입/최초 로그인 시 users/{uid} 문서 생성
+
+//닉네임/프로필 이미지 변경 시 users/{uid} 업데이트
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jobline/features/auth/screens/profile_screen.dart';
 
 // === 각 기능 모듈의 화면 import ===
 import '../features/auth/screens/login_screen.dart';
@@ -10,13 +11,14 @@ import '../features/chat/screens/chat_list_screen.dart';
 
 class AppRoutes {
   // 첫 화면: 로그인 화면으로 지정
-  static const String initial = '/home';
+  static const String initial = '/profile';
 
   // 모든 라우트 등록
   static final Map<String, WidgetBuilder> routes = {
     //'/login': (context) => const LoginScreen(),
-    //'/signup': (context) => const SignupScreen(),
-    '/home': (context) => const HomeScreen(),
+    //'/signup': (context) => const SignUpScreen(),
+    //'/home': (context) => const HomeScreen(),
+    '/profile': (context) => const ProfileScreen(),
     //'/board': (context) => const BoardTabsScreen(),
     //'/calendar': (context) => const CalendarScreen(),
     //'/chat': (context) => const ChatListScreen(),
